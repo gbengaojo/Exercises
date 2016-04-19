81d4b000455efbb991d93447296f24b9c1e9f0b0
Gbenga Ojo <gbenga@gbengaojo.com>
2016-04-19 14:55:42 -0500

    cleaning up

--- reputation_loop/rl-vanilla-php/index.php
+++ reputation_loop/rl-vanilla-php/index.php
@@ -11,6 +11,7 @@ require_once 'classes/ReputationLoop.php';
    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+   <link rel="stylesheet" href="assets/css/style.css">
    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
    <script src="bower_components/moment/min/moment-with-locales.min.js" type="text/javascript"></script>
    
@@ -21,6 +22,7 @@ require_once 'classes/ReputationLoop.php';
          });
       }
 
+      // pagination ajax
       function getReview(page_no) {
          page_no = page_no - 1;
          $.post('assets/ajax/reviews.php?page_no=' + page_no,
@@ -39,44 +41,13 @@ require_once 'classes/ReputationLoop.php';
             }
          );
       }
+
+      $(function() {
+   $('span.stars').stars();
+});
+
    </script>
 
-   <style>
-      html,body {
-         margin: 0px;
-         padding: 0px;
-         overflow-x: hidden; 
-      }
-
-      .container-fluid {
-         margin: 10px;
-         padding: 0px;
-         font-family: 'Helvetica', 'Arial', sans-serif; 
-      }
-
-      header {
-         color: #ccc;
-         background: no-repeat rgb(49, 64, 75);
-         padding-bottom: 20px;
-         padding-left: 10px;
-         padding-top: 5px;
-      }
-
-      .bold {
-         font-weight: bold;
-      }
-
-      span.stars, span.stars span {
-         display: inline-block;
-         background: url(assets/images/stars.png) 0 -16px repeat-x;
-         width: 80px;
-         height: 16px;
-      }
-
-      span.stars span {
-         background-position: 0 0;
-      }
-   </style>
 </head>
 
 <title>Reputation Loop Code Challenge</title>
@@ -113,10 +84,10 @@ require_once 'classes/ReputationLoop.php';
 
       <div class="row">
          <div class="col-md-6">
-            <p><a href="<?php echo $business_info->external_url ?>">External URL</a></p>
+            <p><a href="<?php echo $business_info->external_url ?>">Leave a Review</a></p>
          </div>
          <div class="col-md-6">
-            <p><a href="<?php echo $business_info->external_page_url ?>">External Page URL</a></p>
+            <p><a href="<?php echo $business_info->external_page_url ?>">See All Reviews</a></p>
          </div>
       </div>
    </section>
@@ -161,15 +132,6 @@ require_once 'classes/ReputationLoop.php';
    <!-- end Ratings and Reviews -->
 </div>
 
-<script type="text/javascript">
-$(function() {
-   $('span.stars').stars();
-});
-</script>
-
-
 </body>
 
 </html>
-
-<?php echo '<pre>'; print_r($rl_response); echo '</pre>'; ?>
