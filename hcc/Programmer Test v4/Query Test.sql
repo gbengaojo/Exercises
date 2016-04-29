7b90a12532bed33bdd0680de7e10e4b5bedf4652
Gbenga Ojo <gbenga@gbengaojo.com>
2016-04-29 12:01:04 -0500

    initial commit

---
+++ hcc/Programmer Test v4/Query Test.sql
@@ -0,0 +1,12 @@
+-- Write the following queries
+
+-- 1. Last ten payments by date, with customer name, payment date, check number and amount
+
+-- 2. Vintage Cars larger scale than 1:32
+
+-- 3. Customer Name, Phone, and total number of orders they've place
+
+-- 4.  List of all customers with individual orders over $100
+
+-- 5. List of customers having more than 2 orders, and the date of their most recent order
+
