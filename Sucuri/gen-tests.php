80d38f391a41bdea223da91d9c30e5a4180b3c1e
Gbenga Ojo <gbenga@gbengaojo.com>
2017-02-13 19:31:36 -0600

    random timestamp added

--- Sucuri/gen-tests.php
+++ Sucuri/gen-tests.php
@@ -14,10 +14,12 @@
 $usage_msg = <<<EOT
 
 Usage:
-gen-tests.php
-   Options:
-   -c: generates a CSV file holding common values
-   -f: generates a CSV file holding fringe cases
+gen-tests.php TYPE
+   TYPEs:
+   -c, --common:
+      generates a CSV file holding common values
+   -f, --fringe:
+      generates a CSV file holding fringe cases
 
 
 EOT;
@@ -32,15 +34,20 @@ $prog_name = array_shift($argv);
 while (count($argv) > 0) {
    $arg = array_shift($argv);
 
-   if (preg_match('/^-[h|\-help|\?]/', $arg)) {
+   if (preg_match('/^-[h|\?]/', $arg)) {
       print($usage_msg);
       exit(0);
    }
 
-   elseif (preg_match('/^[\-c|\-\-common]/', $arg))
+   elseif (preg_match('/^-c|^--common/', $arg)) {
       $test_type = "common";
-   elseif (preg_match('/^[\-f|\-\-fringe]/', $arg))
+   } elseif (preg_match('/^-f|^--fringe/', $arg)) {
       $test_type = "fringe";
+   } else {
+      print($usage_msg);
+      exit(0);
+   }
+      
 }
 
 // based on switch (--common or --fringe), generate CSV strings
@@ -64,7 +71,7 @@ function generateFringeData($datatype) {
       break;
 
       case 'timestamp' :
-
+         
       break;
 
       case 'float' :
@@ -89,3 +96,17 @@ function random_str() {
 
    return $str;
 }
+
+function random_timestamp() {
+   $time[] = time() + mt_rand(0, 30) * mt_rand(0, 365) * 24 * 3600;
+   $time[] = 0;
+   $time[] = -1;
+   $time[] = pow(10, 100000000);
+   return $time[mt_rand(0, sizeof($time) - 1)];
+}
+
+
+
+
+echo "test_type: $test_type\n";
+echo random_timestamp() . "\n";
